c62fcaf895ebfa357e4919f6d84ccade10f4a81d
Cornelius_Wang <cory.wang11@gmail.com>
2022-01-25 15:33:32 -0800

    Initialized Branch within Main Activity

--- app/src/main/java/com/corneliuswang/branch_sa_technical_assessment/MainActivity.java
+++ app/src/main/java/com/corneliuswang/branch_sa_technical_assessment/MainActivity.java
@@ -7,6 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import org.json.JSONObject;
+
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
+import io.branch.referral.util.LinkProperties;
+
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.corneliuswang.branch_sa_technical_assessment.Message";
@@ -15,8 +23,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Branch logging for debugging
+        Branch.enableLogging();
 
+        // Branch object initialization
+        Branch.getAutoInstance(this);
     }
+//    @Override
+//    protected void onStart(){
+//        super.onStart();
+//        Branch.sessionBuilder(this).withCallback(branchReferralInitListener).withData(getIntent() != null ? getIntent().getData() : null).init();
+//    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        // if activity is in foreground (or in backstack but partially visible) launching the same
+        // activity will skip onStart, handle this case with reInitSession
+        if (intent != null &&
+                intent.hasExtra("branch_force_new_session") &&
+                intent.getBooleanExtra("branch_force_new_session", true)) {
+            Branch.sessionBuilder(this).withCallback(branchReferralInitListener).reInit();
+        }
+    }
+    private Branch.BranchReferralInitListener branchReferralInitListener = new Branch.BranchReferralInitListener() {
+        @Override
+        public void onInitFinished(JSONObject linkProperties, BranchError error) {
+            // do stuff with deep link data (nav to page, display content, etc)
+        }
+    };
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = findViewById(R.id.editTextTextPersonName);
